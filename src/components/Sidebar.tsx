1ce6d27547f6841841f6c59b06f13408714eab70
castelo <castlogomaa459@gmail.com>
2025-01-30 20:53:53 +0200

    resolve aside z-index be over all elements

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ export default function MainSideBar({
 }) {
     return (
         <aside
-            className={`fixed w-[260px]  md:hidden transition duration-150 bg-color-police-blue h-full top-0 right-0 ${
+            className={`fixed w-[260px]  md:hidden transition duration-150 bg-color-police-blue h-full z-[1111] top-0 right-0 ${
                 sidebarOpenerState
                     ? 'translate-x-0 after:content-[""] after:absolute after:top-0 after:h-full after:md:hidden  after:bg-color-overlay after:w-[1000px] after:right-[260px]'
                     : "translate-x-full"
