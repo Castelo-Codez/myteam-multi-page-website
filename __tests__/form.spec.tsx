ded80d216ce74d8353214491133aee447e5bd22b
castelo <castlogomaa459@gmail.com>
2025-02-12 21:12:16 +0200

    all form fields

--- __tests__/form.spec.tsx
+++ __tests__/form.spec.tsx
@@ -3,7 +3,6 @@ import "@testing-library/jest-dom";
 import Form from "@/components/Form";
 import userEvent from "@testing-library/user-event";
 
-//@ts-expect-error
 describe("testing form component", () => {
     test("test number 1 => user click submit without any info", async () => {
         render(<Form />);
@@ -38,14 +37,79 @@ describe("testing form component", () => {
         await userEvent.type(screen.getByPlaceholderText("Name"), "12315");
         const ErrorPara = screen.getAllByRole("paragraph")[0];
         expect(ErrorPara.textContent).toBe("Name Cannot Be a Number");
-        await userEvent.type(screen.getByPlaceholderText("Name"), "Eslam");
+        await userEvent.type(
+            screen.getByPlaceholderText("Name"),
+            "Eslam Gomaa"
+        );
+        expect(screen.getByPlaceholderText("Name").nextElementSibling).toBe(
+            screen.getByPlaceholderText("Email")
+        );
+    });
+    test("test number 3 => validate Email Input and insure that received Email is Correct", async () => {
+        render(<Form />);
         await userEvent.click(
             screen.getByText("submit", {
                 exact: false,
             })
         );
-        expect(screen.getByPlaceholderText("Name").nextElementSibling).toBe(
-            screen.getByPlaceholderText("Email")
+        expect(screen.getAllByRole("paragraph")[1].textContent).toBe(
+            "Invalid Email"
+        );
+        const EmailInput = screen.getByPlaceholderText("Email");
+        await userEvent.type(EmailInput, "castelo132@.com");
+        expect(screen.getAllByRole("paragraph")[1].textContent).toBe(
+            "Invalid Email"
+        );
+        EmailInput.value = "";
+        await userEvent.type(EmailInput, "castelo132@gmail");
+        expect(screen.getAllByRole("paragraph")[1].textContent).toBe(
+            "Invalid Email"
+        );
+        EmailInput.value = "";
+        await userEvent.type(
+            screen.getByPlaceholderText("Email"),
+            "castelo132@gmail.com"
+        );
+        expect(screen.getByPlaceholderText("Email").nextElementSibling).toBe(
+            screen.getByPlaceholderText("Company")
+        );
+    });
+    test("test number 4 => validate company Input and insure that is not empty and min is 3 and max is 20", async () => {
+        render(<Form />);
+        const titleInput = screen.getByPlaceholderText("Company");
+        await userEvent.click(screen.getByText("Submit"));
+        expect(screen.getAllByRole("paragraph")[3].textContent).toBe(
+            "Must Be At Least 3 Characters"
+        );
+        await userEvent.type(titleInput, "ds");
+        expect(screen.getAllByRole("paragraph")[3].textContent).toBe(
+            "Must Be At Least 3 Characters"
+        );
+        titleInput.value = "";
+        await userEvent.type(titleInput, "twitter");
+        expect(titleInput.nextElementSibling).toBe(
+            screen.getByPlaceholderText("title", {
+                exact: false,
+            })
+        );
+    });
+    test("test number 5 => validate title input and insure that is not empty and min is 3 and max is 20", async () => {
+        render(<Form />);
+        const titleInput = screen.getByPlaceholderText("Title");
+        await userEvent.click(screen.getByText("Submit"));
+        expect(screen.getAllByRole("paragraph")[2].textContent).toBe(
+            "Must Be At Least 3 Characters"
+        );
+        await userEvent.type(titleInput, "ds");
+        expect(screen.getAllByRole("paragraph")[2].textContent).toBe(
+            "Must Be At Least 3 Characters"
+        );
+        titleInput.value = "";
+        await userEvent.type(titleInput, "twitter");
+        expect(titleInput.nextElementSibling).toBe(
+            screen.getByPlaceholderText("message", {
+                exact: false,
+            })
         );
     });
 });
--- __tests__/headerSection.spec.tsx
+++ __tests__/headerSection.spec.tsx
@@ -2,24 +2,23 @@ import "@testing-library/jest-dom";
 import {screen, render} from "@testing-library/react";
 import HeaderSection from "@/components/headerSection";
 import useEvent from "@testing-library/user-event";
-//@ts-expect-error
+
 describe("test header section", () => {
-    //@ts-expect-error
     test("check the content", () => {
-        render(<HeaderSection />); //@ts-expect-error
-        expect(screen.getByTestId("header")).toBeInTheDocument(); //@ts-expect-error
+        render(<HeaderSection />);
+        expect(screen.getByTestId("header")).toBeInTheDocument();
         expect(screen.getByAltText("logo")).toBeInTheDocument();
-    }); //@ts-expect-error
+    });
     test("check clicking in the hamburger button will show the sidbar", async () => {
         const headerSec = render(<HeaderSection />);
         const user = useEvent.setup();
         const sidebarOpener = headerSec.getByTitle("open sidebar");
-        const sidebar = headerSec.getByTestId("aside-1"); //@ts-expect-error
-        expect(sidebarOpener).toBeInTheDocument(); //@ts-expect-error
-        expect(sidebar).toBeInTheDocument(); //@ts-expect-error
-        expect(sidebarOpener).toBeVisible(); //@ts-expect-error
+        const sidebar = headerSec.getByTestId("aside-1");
+        expect(sidebarOpener).toBeInTheDocument();
+        expect(sidebar).toBeInTheDocument();
+        expect(sidebarOpener).toBeVisible();
         expect(sidebar).not.toBeVisible();
-        await user.click(sidebarOpener); //@ts-expect-error
+        await user.click(sidebarOpener);
         expect(sidebar).toBeVisible();
     });
 });
